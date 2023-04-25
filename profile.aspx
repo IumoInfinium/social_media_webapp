34de7f557df129f757d0dd2c01496f6f74feee1e
IumoInfinium <iumoinfinium1331@gmail.com>
2023-04-25 13:08:20 +0530

    re-added profile files

---
+++ profile.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="profile.aspx.cs" Inherits="profile" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+        <link rel="stylesheet" href="./assets/css/profile.css"/>
+        <link rel="stylesheet" href="./assets/csss/style.css" />
+        <link rel="shortcut icon" href="./assets/imgs/instagram.png" type="image/x-icon">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <section class="profile">
+        <div class="profile-photo">
+            <img src="./assets/imgs/profile-photo.png" alt="">
+        </div>
+
+        <section class="profile-user">
+            <div class="settings">
+
+                <asp:Label ID="Label1" runat="server" Text="yashm" class="user-name"></asp:Label>
+                <asp:Button ID="Button1" runat="server" Text="Edit Profile" class="edit-button" OnClick="Button1_Click"/>
+                
+<%--                <asp:LinkButton ID="LinkButton2" runat="server" CssClass="follow-button">
+                    Follow
+                </asp:LinkButton>--%>
+
+                    <asp:LinkButton ID="LinkButton1" runat="server" class="settings-button">
+                        <img src="./assets/imgs/setting.png"  class="setting-button-img "alt="">    
+                    </asp:LinkButton>
+            </div>
+            <br />
+            
+            <div class="posts-followers-following">
+                <ul>
+                    <li>
+                        <asp:Label ID="Label2" runat="server" Text=""  class="bold"></asp:Label>
+                        posts</li>
+                    <li>
+                        <asp:Label ID="Label3" runat="server" Text=""  class="bold"></asp:Label>
+                        followers</li>
+                    <li>
+                        <asp:Label ID="Label4" runat="server" Text=""  class="bold"></asp:Label>
+                        following</li>
+                </ul>
+            </div>
+           <br />
+            <div class="name">
+                <div class="user-info">
+                    
+                    <asp:TextBox ID="TextBox1" runat="server"  class="user-profile-info" Text=""  ></asp:TextBox>
+                    <asp:TextBox ID="TextBox2" runat="server" class="user-profile-info" Text=""  ></asp:TextBox>
+                    <asp:TextBox ID="TextBox3" runat="server" class="user-profile-info" Text="" ></asp:TextBox>
+                
+                    <asp:TextBox ID="TextBox4" runat="server" class="user-profile-info" Text="" ></asp:TextBox>
+                </div>
+            </div>
+         
+        </section>
+    </section>
+
+    <!--Main-->
+    <main>
+        <section class="options">
+            <ul>
+                <li>Posts</li>
+                <li>Reels</li>
+                <li>Videos</li>
+                <li>Saved</li>
+                <li>Tagged</li>
+            </ul>
+        </section>
+
+        <section class="feed">
+            <ul style="list-style-type:none">
+                <asp:Repeater ID ="r1" runat ="server">
+                    <ItemTemplate>
+                        <li>
+                            <div class="user-profile-feed">
+                                <asp:Image  ID="Image1" runat="server" ImageUrl='<%# Eval("postURL") %>' alt="" /> 
+                                <asp:LinkButton ID="LinkButton2" runat="server" OnCommand="GoToPost" CommandName="gotopost" CommandArgument='<%# Eval("postid") %>'>Go to Post</asp:LinkButton>
+                            </div>
+                        </li>
+                    </ItemTemplate>
+                </asp:Repeater>
+                
+            </ul>
+        </section>
+    </main>
+</asp:Content>
+
---
+++ profile.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class profile : System.Web.UI.Page
+{
+    SqlConnection conn;
+    SqlDataAdapter ad;
+    SqlCommand cmd;
+
+    string curr_user;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["username"] == null)
+        {
+            if (Session["username"] != null) curr_user = Session["username"].ToString();
+            else Response.Redirect("login.aspx");
+        }
+        else curr_user = Request.QueryString["username"].ToString();
+
+        string path = ConfigurationManager.ConnectionStrings["connect"].ToString();
+        conn = new SqlConnection(path);
+        conn.Open();
+
+        if (!IsPostBack)
+        {
+            string query = "select * from accounts where username = '" + curr_user + "'";
+            cmd = new SqlCommand(query, conn);
+
+            SqlDataAdapter ad = new SqlDataAdapter(query, conn);
+            DataSet ds = new DataSet();
+
+            ad.Fill(ds);
+
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                Response.Write("<script> alert('No user found with such username!') </script>");
+                Response.Redirect("feed.aspx");
+            }
+            else
+            {
+                Label1.Text = curr_user;
+
+                TextBox1.Text = ds.Tables[0].Rows[0][1].ToString();
+                TextBox2.Text = ds.Tables[0].Rows[0][2].ToString();
+                TextBox3.Text = ds.Tables[0].Rows[0][3].ToString();
+                TextBox4.Text = ds.Tables[0].Rows[0][4].ToString();
+
+                Label3.Text = ds.Tables[0].Rows[0][7].ToString();
+                Label4.Text = ds.Tables[0].Rows[0][8].ToString();
+
+                string query2 = "select * from posts where user_id= (SELECT id from accounts where username='" + curr_user + "') order by postOn desc";
+
+                SqlDataAdapter ad2 = new SqlDataAdapter(query2, conn);
+                DataSet ds2 = new DataSet();
+                ad2.Fill(ds2);
+
+                Label2.Text = ds2.Tables[0].Rows.Count.ToString();
+                r1.DataSource = ds2;
+                r1.DataBind();
+            }
+        }
+        //reader.Close();
+
+        if (((Session["username"]!=null && Request.QueryString["username"] != null)  && Request.QueryString["username"] == Session["username"].ToString())
+            ||
+            (Session["username"] != null && Request.QueryString["username"]  == null)
+            )
+        {
+
+            TextBox1.Attributes.Remove("disabled");
+            TextBox2.Attributes.Remove("disabled");
+            TextBox3.Attributes.Remove("disabled");
+            TextBox4.Attributes.Remove("disabled");
+
+            TextBox1.Style.Add("border-bottom", "1px solid gray");
+            TextBox2.Style.Add("border-bottom", "1px solid gray");
+            TextBox3.Style.Add("border-bottom", "1px solid gray");
+            TextBox4.Style.Add("border-bottom", "1px solid gray");
+        }
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        if ((Session["username"] != null && Request.QueryString["username"]!= null &&  Session["username"].ToString() == Request.QueryString["username"].ToString())
+            ||(Session["username"] !=null && Request.QueryString["username"] == null)
+            )
+        {
+            string query = "update accounts set name='" + TextBox1.Text + "', number= '" + TextBox2.Text + "', email = '" + TextBox3.Text + "', bio = '" + TextBox4.Text + "' where username= '" + curr_user + "'";
+            cmd = new SqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+
+    protected void GoToPost(object sender, CommandEventArgs e)
+    {
+        if (e.CommandName == "gotopost") {
+            Response.Redirect("post.aspx?postid=" + e.CommandArgument);
+        }
+    }
+}
